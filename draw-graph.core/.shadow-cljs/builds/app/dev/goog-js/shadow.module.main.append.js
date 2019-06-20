18e437bad4b449c2828210ea66bb6e929721ecfa
Jude Payne <judepayne@yahoo.co.uk>
2019-06-20 07:03:58 +0100

    changed .gitignore

--- draw-graph.core/.shadow-cljs/builds/app/dev/goog-js/shadow.module.main.append.js
+++
@@ -1,1 +0,0 @@
-["^ ","~:resource-id",["~:shadow.build.modules/append","~:main"],"~:source","\nshadow.cljs.devtools.client.browser.module_loaded('main');\n","~:js","\nshadow.cljs.devtools.client.browser.module_loaded('main');\n"]
